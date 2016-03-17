7ea61c11f9be3fe946a214366f729552296cfd1d
Kahbukie <kahbukie001@gmail.com>
2016-03-17 13:01:15 -0430

    tossing double dice

---
+++ Dicedoubles.java
@@ -0,0 +1,24 @@
+ import java.util.Random;
+public class Dicedoubles 
+{
+    public static void main( String[] args )
+    {
+      Random r = new Random();
+      
+        System.out.print(" HERE COMES THE DICE!");
+        int Roll1 = 1 + r.nextInt(6);
+        int Roll2 = 1 + r.nextInt(6);
+        
+        while ( Roll1 != Roll2 )
+        {  
+        System.out.print( "\nRoll # 1: " + Roll1);
+        System.out.println( "\nRoll # 2: " + Roll2);
+        
+        System.out.println("The total is");
+        System.out.println( Roll1 + Roll2);
+        }
+       // System.out.print( "they are equal");
+
+    } 
+   
+}
